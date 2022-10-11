8913b28cb1a7663e4e2cb9807a8510520b0a28a2
Swangi Kumari <85639103+swangi-kumari@users.noreply.github.com>
2022-10-11 15:06:15 +0530

    Create 1323. Maximum 69 Number.java

---
+++ LeetCode Easy/1323. Maximum 69 Number.java
@@ -0,0 +1,53 @@
+/**
+1323. Maximum 69 Number
+You are given a positive integer num consisting only of digits 6 and 9.
+
+Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+
+Example 1:
+
+Input: num = 9669
+Output: 9969
+Explanation: 
+Changing the first digit results in 6669.
+Changing the second digit results in 9969.
+Changing the third digit results in 9699.
+Changing the fourth digit results in 9666.
+The maximum number is 9969.
+Example 2:
+
+Input: num = 9996
+Output: 9999
+Explanation: Changing the last digit 6 to 9 results in the maximum number.
+Example 3:
+
+Input: num = 9999
+Output: 9999
+Explanation: It is better not to apply any change.
+**/
+
+class Solution {
+    public int count (int n){
+        int count = -1;
+        while(n>0){
+            n = n/10;
+            count++;
+        }
+        return count;
+    }
+    
+    public int maximum69Number (int num) {
+        int res = num;
+        int c = count(num);
+        for(int i = (int)Math.pow(10,c); i>0; i=i/10){
+
+            if((num/i)==6){
+                
+                int ans = res+(i*3);
+                return ans;
+            }
+            num = num-(i*9);
+        }
+        return res;
+    }
+}
