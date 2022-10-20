b27821fb9d71d63c26a81619b650e57fcc2a7523
Swangi Kumari <85639103+swangi-kumari@users.noreply.github.com>
2022-10-20 23:51:24 +0530

    Update 1. Two Sum.java

--- LeetCode Easy/1. Two Sum.java
+++ LeetCode Easy/1. Two Sum.java
@@ -27,6 +27,7 @@ class Solution {
     public int[] twoSum(int[] nums, int x) {
         int n = nums.length;
         int arr[] = new int[2];
+        
         for(int i = 0; i < n-1; i++){
             for(int j = i+1; j < n; j++){
                 if(nums[i]+nums[j]==x){
