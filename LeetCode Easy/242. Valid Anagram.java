757bf05f39d17308362a2bb5c94f45e7264dbb3f
Swangi Kumari <85639103+swangi-kumari@users.noreply.github.com>
2022-11-13 13:41:04 +0530

    Create 242. Valid Anagram.java

---
+++ LeetCode Easy/242. Valid Anagram.java
@@ -0,0 +1,38 @@
+/**
+242. Valid Anagram
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+
+**/
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int[] count = new int[26];
+        for(int i = 0; i<s.length(); i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        
+        for(int i = 0; i < count.length; i++){
+            if(count[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
